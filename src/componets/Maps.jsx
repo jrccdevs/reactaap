d7a78bf2da79e658066935b0d04b638262c02169
RodrigoCG <rodrig127g@gmail.com>
2023-01-13 15:21:47 -0400

    mapa de sucursales de alfa RESPONSIVE

--- src/componets/Maps.jsx
+++ src/componets/Maps.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Modal, Button, Container, Row, Col } from "react-bootstrap";
 import mapa from "../img/mapa-sucursales.png";
+import ImageMapResizer from 'image-map-resizer';
+
 
 export default function Maps() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -18,6 +20,9 @@ export default function Maps() {
     setModalData(null);
   };
 
+  useEffect(() => {
+    ImageMapResizer();
+  }, []);
   return (
     <div className="container">
       <div className="row">
@@ -25,7 +30,7 @@ export default function Maps() {
           <div className="col-lg-12 text-center">
             <h4 className="text-center my-4">SUCURSALES BOLIVIA</h4>
 
-            <img className="img-fluid" src={mapa} width="652" height="682" alt="sucursales-bolivia" useMap="#Map" />
+            <img className="img-fluid" src={mapa} alt="sucursales-bolivia" useMap="#Map" />
             <map name="Map">
               <area
                 shape="circle"
@@ -66,7 +71,7 @@ export default function Maps() {
                 alt="la_paz"
                 onClick={(event) => openModal(event, {
                   title: "Sucursal La Paz",
-                  body: "Información de la sucursal de La Paz",
+                  body: "Avda. Bush Nº 1970 (entre Rosendo Villalobos y Díaz Romero), Miraflores.",
                 })}
               />
 
@@ -132,9 +137,24 @@ export default function Maps() {
       {modalData && (
         <Modal show={modalOpen} onHide={closeModal} aria-labelledby="contained-modal-title-vcenter" centered size="lg">
           <Modal.Header closeButton>
-            <Modal.Title>{modalData.title}</Modal.Title>
+            <Modal.Title>
+              {modalData.title}
+            </Modal.Title>
           </Modal.Header>
-          <Modal.Body>{modalData.body}</Modal.Body>
+          {/* <Modal.Body>{modalData.body}</Modal.Body> */}
+          <Modal.Body>
+            <Container>
+              <Row>
+                <Col className="bg-danger" xs={6} md={6}>
+                  <p>{modalData.body}</p>
+                </Col>
+                <Col className="bg-info" xs={6} md={6}>
+                  .col-xs-6 .col-md-4
+                </Col>
+
+              </Row>
+            </Container>
+          </Modal.Body>
           <Modal.Footer>
             <Button variant="primary" onClick={closeModal}>
               Cerrar
@@ -148,72 +168,3 @@ export default function Maps() {
   );
 }
 
-// import React, { useState } from "react";
-// import { Modal, Button } from "react-bootstrap";
-// import { ImageMap } from '@qiuz/react-image-map';
-// import mapa from "../img/mapa-sucursales.png";
-
-// export default function Maps() {
-//   const [modalOpen, setModalOpen] = useState(false);
-//   const [modalData, setModalData] = useState(null);
-
-//   const openModal = (event, data) => {
-//     event.preventDefault();
-//     setModalOpen(true);
-//     setModalData(data);
-//   };
-
-//   const closeModal = () => {
-//     setModalOpen(false);
-//     setModalData(null);
-//   };
-
-//   const shapes = [
-//     {
-//       shape: "circle",
-//       coords: [417, 319, 17],
-//       alt: "santa_cruz",
-//       onClick: (event) =>
-//         openModal(event, {
-//           title: "Sucursal Santa Cruz",
-//           body: "Información de la sucursal de Santa Cruz",
-//         }),
-//     },
-//     {
-//       shape: "circle",
-//       coords: [236, 182, 17],
-//       alt: "beni",
-//       onClick: (event) =>
-//         openModal(event, {
-//           title: "Sucursal Beni",
-//           body: "Información de la sucursal de Beni",
-//         }),
-//     },
-//   ];
-
-//   return (
-//     <div className="container">
-//       <div className="row">
-//         <section className="col-12">
-//           <ImageMap className="img-fluid" src={mapa} width={652} height={682} shapes={shapes} />
-//           <img className="img-fluid" src={mapa} width="652" height="682" alt="sucursales-bolivia" useMap="#Map" />
-
-//         </section>
-//       </div>
-
-//       {modalData && (
-//         <Modal show={modalOpen} onHide={closeModal} aria-labelledby="contained-modal-title-vcenter" centered size="lg">
-//           <Modal.Header closeButton>
-//             <Modal.Title>{modalData.title}</Modal.Title>
-//           </Modal.Header>
-//           <Modal.Body>{modalData.body}</Modal.Body>
-//           <Modal.Footer>
-//             <Button variant="primary" onClick={closeModal}>
-//               Cerrar
-//             </Button>
-//           </Modal.Footer>
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
