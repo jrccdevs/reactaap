f0e14388f6e2fac1a684b3bb7d2a8e89654ae52d
rei <reynaldocalani80@gmail.com>
2023-12-20 14:49:29 -0400

    links de los banner marginBottom

--- src/componets/PaginaCarrucel.jsx
+++ src/componets/PaginaCarrucel.jsx
@@ -40,7 +40,7 @@ function PaginaCarrucel() {
                 
             
                 <img
-                    style={{marginLeft:"1%", marginTop:"5%", width: "100%", height: "auto" }}
+                    style={{marginLeft:"1%", marginTop:"5%", width: "100%", height: "auto", marginBottom:"45px" }}
                     className="img-productos"
                     src={producto.archivo}
                     alt=""
